8dbe6dc9e1cf41dc6859cfd55ac4f166fb2680d1
antoine <zaravelas@outlook.de>
2021-10-11 14:05:57 +0200

    finished

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 class Game {
     constructor() {
-        this.score = 0;
         this.turn = true;
         // true means it's O's turn
         this.boxesArray = [
@@ -11,6 +10,7 @@ class Game {
         this.value = false;
         this.scoreFirst = 0;
         this.scoreSecond = 0;
+        this.canIPlay = true;
     }
 
     restart() {
@@ -23,8 +23,22 @@ class Game {
         ]
     }
 
+    reset() {
+        this.turn = true;
+        // true means it's O's turn
+        this.boxesArray = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+        this.value = false;
+        this.scoreFirst = 0;
+        this.scoreSecond = 0;
 
-    checking(player1,player2) {
+    }
+
+
+    checking(player1, player2) {
         // i will check if someone won
         let value1 = this.boxesArray.every(ar => ar[0] == "O");
         let value2 = this.boxesArray.every(ar => ar[1] == "O");
@@ -40,11 +54,16 @@ class Game {
         let value12 = [this.boxesArray[0][2], this.boxesArray[1][1], this.boxesArray[2][0]].every(x => x == "X");
         this.value = value1 | value2 | value3 | value4 | value5 | value6 | value7 | value8 | value9 | value10 | value11 | value12;
 
-        console.log("after reset the board is ", this.boxesArray);
         if (this.value) {
             if (value1 | value2 | value3 | value8 | value9 | value10) player1.innerHTML++;
             else player2.innerHTML++;
+            document.getElementById("winner").innerHTML = `We have a winner!😎🎇`;
+
+            // game has to stop when someone wins, the other boxes will stop being clickable
+            this.canIPlay = false;
         }
+        console.log("can you play? ", this.canIPlay);
+
 
     }
 
@@ -67,52 +86,85 @@ class Game {
     }
 }
 
+// making an instance of a game 
+
+let newGame = new Game();
+
+// -----
+
+
 let startTheGame = document.getElementsByClassName("start")[0];
 let boxes = document.getElementsByClassName("box");
-
-
+let scorePlayer1 = document.getElementsByClassName("score")[0];
+let scorePlayer2 = document.getElementsByClassName("score")[1];
 
 
 function starting() {
-
     // Starting the Game - clicking O and X
     let player1 = prompt("What's the name of the first player?");
     let player2 = prompt("What's the name of the second player?");
     document.getElementById("#player1").innerHTML = player1;
     document.getElementById("#player2").innerHTML = player2;
-    document.getElementsByClassName("announcement1")[0].innerHTML=`The player to be first is ${player1}`;
-    document.getElementsByClassName("announcement2")[0].innerHTML=`The player to be second is ${player2}`;
-    let newGame = new Game();
-    let scorePlayer1 = document.getElementsByClassName("score")[0];
-    let scorePlayer2 = document.getElementsByClassName("score")[1];
+    document.getElementsByClassName("announcement1")[0].innerHTML = `The player to be first is ${player1}`;
+    document.getElementsByClassName("announcement2")[0].innerHTML = `The player to be second is ${player2}`;
+
     scorePlayer1.innerHTML = newGame.scoreFirst;
     scorePlayer2.innerHTML = newGame.scoreSecond;
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].addEventListener("click", () => {
-            if (newGame.turn) {
-                boxes[i].innerHTML = "O";
-                newGame.fillArray(i, boxes[i].innerHTML);
-                newGame.checking(scorePlayer1,scorePlayer2);
-            } else {
-                boxes[i].innerHTML = "X";
-                newGame.fillArray(i, boxes[i].innerHTML);
-                newGame.checking(scorePlayer1,scorePlayer2);
-            }
-            newGame.turn = !newGame.turn;
-            // this.checking();
-        })
+
+    if (newGame.canIPlay) {
+        for (let i = 0; i < boxes.length; i++) {
+            boxes[i].addEventListener("click", () => {
+                if (newGame.canIPlay) {
+                    if (newGame.turn && boxes[i].innerHTML.length==0) {
+                        boxes[i].innerHTML = "O";
+                        newGame.fillArray(i, boxes[i].innerHTML);
+                        newGame.checking(scorePlayer1, scorePlayer2)
+                    } else if (!newGame.turn && boxes[i].innerHTML.length==0) {
+                        boxes[i].innerHTML = "X";
+                        newGame.fillArray(i, boxes[i].innerHTML);
+                        newGame.checking(scorePlayer1, scorePlayer2)
+                    }
+                }
+                newGame.turn = !newGame.turn;
+                // this.checking();
+            })
+        }
     }
 
 
-    // Restarting the Game
 
-    document.getElementsByClassName("restart")[0].addEventListener("click", () => {
-        newGame.restart();
-        for (let i = 0; i < boxes.length; i++) {
-            boxes[i].innerHTML = "";
-        }
-    })
+
+
 }
 
+// play more
 
-startTheGame.addEventListener("click", starting)
+document.getElementsByClassName("restart")[0].addEventListener("click", () => {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].innerHTML = "";
+    }
+    document.getElementById("winner").innerHTML = ``;
+    newGame = new Game();
+    console.log(` the newGame instance after next round will start to be`, newGame)
+})
+
+
+
+// Resetting the game
+
+
+document.getElementById("reset").addEventListener("click", () => {
+
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].innerHTML = "";
+    }
+    document.getElementById("winner").innerHTML = ``;
+    scorePlayer1.innerHTML = 0;
+    scorePlayer2.innerHTML = 0;
+    newGame = new Game();
+})
+
+
+startTheGame.addEventListener("click", starting, {
+    once: true
+});
